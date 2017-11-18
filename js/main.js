acbae8520fae4269fb02bb8573a53a6bf1cdc997
Larisa <l90vegeta@gmail.com>
2017-11-18 21:33:31 +0100

    fixed scrollspy

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$("#main-nav").children().click(function() {
-    $( "li" ).each(function() {
-        $( this ).removeClass( "active" );
-    });
-    $(this).addClass("active");
-});
+// $("#main-nav").children().click(function() {
+//     $( "li" ).each(function() {
+//         $( this ).removeClass( "active" );
+//     });
+//     $(this).addClass("active");
+// });
 
 $(function () {
     $(document).scroll(function () {
@@ -11,3 +11,46 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+// Add smooth scrolling on all links inside the navbar
+$("#mainHeader li:not(:first-child) a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        
+    } // End if
+
+});
+
+$("#mainHeader li:first-child a").on('click', function(event){
+        
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate({
+        scrollTop: 0
+    }, 800, function(){
+
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+
+    });    
+})
